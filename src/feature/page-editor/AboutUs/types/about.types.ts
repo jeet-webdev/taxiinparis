85812c29c2ba8568f46db79ee170b96fe8a407ee
jeet <jeetdev1988@gmail.com>
2026-02-25 18:58:34 +0530

    Update about.types.ts

--- src/feature/page-editor/AboutUs/types/about.types.ts
+++ src/feature/page-editor/AboutUs/types/about.types.ts
@@ -1,6 +1,6 @@
 export type AboutPageFormValues = {
   title: string;
-  AboutheaderImage: File | string | null;
+  headerImage: File | string | null;
   content: string;
   metaTitle: string;
   metaDescription: string;
