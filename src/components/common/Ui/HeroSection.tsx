38815d3694f8251cf043d1a3eb5b087b4572fe8d
ramjeet104 <jeetdev1988@gmail.com>
2026-02-27 17:14:26 +0530

    Frontend task

--- src/components/common/Ui/HeroSection.tsx
+++ src/components/common/Ui/HeroSection.tsx
@@ -10,35 +10,30 @@ export default function HeroSection({ img }: HeroSectionProps) {
       {/* Background Image */}
       <Image
         src={img ?? "/assets/images/hero-img.jpeg"}
-        
         alt="Luxury Car"
         fill
         priority
-        className="object-cover object-center"
+        className="
+    object-cover object-center
+    max-[640px]:!relative
+    max-[640px]:!h-[250px]
+  "
       />
 
       {/* Content Container */}
       <div className="relative z-10 max-w-7xl mx-auto min-h-screen flex flex-col md:flex-row items-center justify-end px-4 md:px-6 py-6 md:py-10">
         <div
           className="
-      w-full
-      taxi-calculator
-      max-w-md
-      h-[85vh] 
-      md:h-[690px]
-      rounded-2xl
-      overflow-hidden
-      shadow-[0_20px_50px_rgba(0,0,0,0.5)]
-      border border-white/10
-      bg-white
-      relative
-    "
+  w-full md:w-[375px] h-[710px]
+  rounded-[12px]
+  taxi-calculator
+"
         >
           <iframe
-             src="https://portail.driverconnect.fr/vtc-fils/template?src=se&tkn=00001_2769650_-1157023572_1772012786065"
+            src="https://portail.driverconnect.fr/vtc-fils/template?src=se&tkn=00001_2769650_-1157023572_1772012786065"
             allow="geolocation"
             title="Booking Widget"
-            className="w-full h-full border-none"
+            className="w-full md:w-[375px] h-[710px] border-none rounded-[12px]"
           />
         </div>
       </div>
