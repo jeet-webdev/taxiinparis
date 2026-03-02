bb859f16c5b4c838d7c9d8be4b673c28e57242ae
jeet <jeetdev1988@gmail.com>
2026-03-02 16:39:14 +0530

    termspage-bug-fixed

--- src/actions/page/getPage.ts
+++ src/actions/page/getPage.ts
@@ -3,7 +3,8 @@
 import { prisma } from "@/src/lib/prisma";
 
 export async function getPageBySlug(slug: string) {
-  return prisma.page.findFirst({
+  // return prisma.page.findFirst({ //findUnique
+  return prisma.page.findUnique({
     where: { slug },
   });
 }
