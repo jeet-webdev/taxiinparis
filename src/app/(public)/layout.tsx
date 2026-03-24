4629e9b08955a8419308672750c263b5748bc3e2
jeet <jeetdev1988@gmail.com>
2026-03-24 16:26:37 +0530

    whatsapp icon removed

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -1,6 +1,6 @@
 import AppLayout from "@/src/components/common/Layout/AppLayout";
 import TawkTo from "@/src/components/common/Layout/TawkTo";
-import WhatsAppButton from "@/src/components/common/Layout/WhatsAppButton";
+// import WhatsAppButton from "@/src/components/common/Layout/WhatsAppButton";
 import { headers } from "next/headers";
 
 export default async function PublicLayout({
@@ -20,7 +20,7 @@ export default async function PublicLayout({
   return (
     <AppLayout>
       {children}
-      <WhatsAppButton />
+      {/* <WhatsAppButton /> */}
       <TawkTo />
     </AppLayout>
   );
