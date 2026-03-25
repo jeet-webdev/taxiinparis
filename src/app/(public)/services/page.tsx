6ef18fd069ed5e3bb6f5d7cc45127e216bd5e161
jeet <jeetdev1988@gmail.com>
2026-03-25 13:38:19 +0530

    service page done

--- src/app/(public)/services/page.tsx
+++ src/app/(public)/services/page.tsx
@@ -44,8 +44,9 @@ export default async function ServicesPage() {
   return (
     <>
       <HeroSection img={page?.imageUpload || undefined} alt={page?.imageAlt} />
-      <TestimonialCard />
+
       <DarkLuxuryBlock>
+        <TestimonialCard />
         <Content data={{ title: page?.title, content: page?.content }} />
         <TestimonialsSection />
       </DarkLuxuryBlock>
